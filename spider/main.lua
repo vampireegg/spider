dfc967fac6236d5d285089b31b5d9b8961a151df
nayeem <vampireegg@gmail.com>
2017-12-11 13:08:11 +0600

    spider vars

--- spider/main.lua
+++ spider/main.lua
@@ -132,7 +132,7 @@ local spider = display.newGroup()
 spider.x = 300
 spider.y = 200
 
-local colliderSize = 100
+local colliderHeight = 35
 local colliderWidth = 70
 local colliderGroup = display.newGroup()
 colliderGroup.x = 150
@@ -140,34 +140,36 @@ colliderGroup.y = 400
 local collider = {}
 local function drawCollider(n)
 	for i = 0,n - 1 do
-		collider[i] = display.newImageRect( "collider2.png", colliderSize,colliderSize  )
+		collider[i] = display.newImageRect( "collider.png", colliderWidth,colliderHeight  )
 		collider[i].x = i * colliderWidth
 		colliderGroup:insert(collider[i])
-		collider[i].colliderRectParams = { halfWidth=35, halfHeight=50, x=colliderGroup.x + collider[i].x , y=colliderGroup.y + collider[i].y, angle=0 }
+		collider[i].colliderRectParams = { halfWidth=colliderWidth/2, halfHeight=colliderHeight/2, x=colliderGroup.x + collider[i].x , y=colliderGroup.y + collider[i].y, angle=0 }
 	end
 	colliderGroup.width = colliderWidth * n
 end
 drawCollider(5)
-
+local arrowsize = 24
+local bodysize = 140
+local arrowDistance = 105
+local spiderRadius = arrowDistance + arrowsize
 local leg = {}
 for i = 1,8 do
-	leg[i] = display.newImageRect( "arrow.png", 24,24  )
+	leg[i] = display.newImageRect( "arrow.png", arrowsize,arrowsize  )
 	leg[i].angle = i * 45
 	leg[i].isBullet = true
 	leg[i].radAngle = (leg[i].angle + 135) * math.pi / 180
 	leg[i].exists = true
 	leg[i].rotAngle = (leg[i].angle + 315) * math.pi / 180
-	local distance = 105
 	spider:insert( leg[i] )	
 	
 	leg[i].rotation = leg[i].angle
-	leg[i]:translate (distance * math.cos(leg[i].rotAngle), distance * math.sin(leg[i].rotAngle))
+	leg[i]:translate (arrowDistance * math.cos(leg[i].rotAngle), arrowDistance * math.sin(leg[i].rotAngle))
 	display.save( leg[i],{filename="cleg"..i..".png", baseDir="E:" } )
 	
 	
 end
 
-local body = display.newImageRect( "body2.png", 140, 140 )
+local body = display.newImageRect( "body2.png", bodysize, bodysize )
 spider:insert( body )
 spider.isFixedRotation = true
 
@@ -189,11 +191,14 @@ local function pushLeg(event )
 	
 end
 
+--spider.RectParams = { halfWidth=spiderRadius, halfHeight=spiderRadius, x=spider.x , y=spider.y, angle=0 }
+
 physics.start()
 physics.setGravity( 0, 0)
 
 --physics.addBody( body, "dynamic", {radius = 100} )
-physics.addBody( spider, "dynamic", {radius = 90})
+--physics.addBody( spider, "dynamic", { box = spider.RectParams, friction=0, bounce=0})
+physics.addBody( spider, "dynamic", {radius = spiderRadius})
 
 --local colliderRectParams = { halfWidth=140, halfHeight=35, x=colliderGroup.x, y=colliderGroup.y, angle=0 }
 --physics.addBody( colliderGroup, "static", { friction=0, bounce=0} )

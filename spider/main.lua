c517f4175f1575a835c47f33015cff09c754193a
nayeem <vampireegg@gmail.com>
2017-12-11 11:47:24 +0600

    borders added

--- spider/main.lua
+++ spider/main.lua
@@ -102,32 +102,30 @@ local spiral = {}
 for i = 1,10 do
 	local x = math.random(0, totalHeight)
 	local y = math.random(0, totalWidth)
-	--spiral[i] = display.newImageRect( "spiral.png", 15,15  )
-	--spiral[i].x = x
-	--spiral[i].y = y
-	--spiral[i]:setFillColor( 1, 1, 1, .7 )
+	-- spiral[i] = display.newImageRect( "spiral.png", 20,20  )
+	-- spiral[i].x = x
+	-- spiral[i].y = y
+	-- spiral[i]:setFillColor( 1, 1, 1, .2 )
+end
+
+local borderWidth = 30
+local borders = {}
+local function drawBorder()
+	borders[0] = display.newRect( borderWidth/2, totalWidth/2, borderWidth, totalWidth)
+	borders[0]:setFillColor( 1, 1, 1, 0 )
+	
+	borders[1] = display.newRect( totalHeight - borderWidth/2,  totalWidth/2, borderWidth, totalWidth)
+	borders[1]:setFillColor( 1, 1, 1, 0 )
+	
+	borders[2] = display.newRect( totalHeight/2, borderWidth/2, totalHeight, borderWidth)
+	borders[2]:setFillColor( 1, 1, 1, 0 )
+
+	borders[3] = display.newRect( totalHeight/2, totalWidth - borderWidth/2, totalHeight, borderWidth)
+	borders[3]:setFillColor( 1, 1, 1, 0 )
 end
 
 
-
-local function drawHelpBorder()
-	local gradient = 
-	{
-		type="gradient",
-		color1={ 0, 0, 0 }, color2={ .6, .6, .6 }, direction="down"
-	}
-	local color =
-	{
-		r = 0,
-		g = 0,
-		b = 0
-	}
-	drawShadowedLine(625,75,785,75, 4, color, gradient)
-
-end
-
-
---drawHelpBorder()
+drawBorder()
 
 
 local spider = display.newGroup()
@@ -181,8 +179,7 @@ local function pushLeg(event )
 	
 	
 	local vx, vy = spider:getLinearVelocity()
-	if(vx == 0 and vy == 0)
-	then
+	if(vx == 0 and vy == 0) then
 		leg:removeSelf()
 		--spider:setLinearVelocity( 50 * math.cos(leg.radAngle), 50 * math.sin(leg.radAngle))
 		spider:applyLinearImpulse( 3 * math.cos(leg.radAngle), 3 * math.sin(leg.radAngle), body.x , body.y )
@@ -199,6 +196,10 @@ physics.addBody( spider, "dynamic", {radius = 90})
 
 local colliderRectParams = { halfWidth=140, halfHeight=35, x=colliderGroup.x, y=colliderGroup.y, angle=0 }
 physics.addBody( colliderGroup, "static", { friction=0, bounce=0} )
+
+for i = 0,3 do
+	physics.addBody( borders[i], "static", { friction=0, bounce=0} )
+end
 
 for i = 1,8 do
 	--local offsetRectParams = { halfWidth=35, halfHeight=35, x=leg[i].x, y=leg[i].y, leg[i].angle }

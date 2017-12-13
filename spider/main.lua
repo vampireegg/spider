4a35a0b1bce31ecff5d9d172c8119b1599979ca6
nayeem <vampireegg@gmail.com>
2017-12-13 14:52:40 +0600

    new main

---
+++ spider/main.lua
@@ -0,0 +1,12 @@
+
+
+local composer = require( "composer" )
+ 
+-- Hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+ 
+-- Seed the random number generator
+math.randomseed( os.time() )
+ 
+-- Go to the menu screen
+composer.gotoScene( "level" )

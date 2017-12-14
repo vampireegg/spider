ebb5e9d14549a1e3961dc6f3d7d6f91ef6bdc322
nayeem <vampireegg@gmail.com>
2017-12-14 20:16:44 +0600

    lvl3 fixes

--- spider/levelProp.lua
+++ spider/levelProp.lua
@@ -68,14 +68,14 @@ M[3] = {}
 
 
 M[3].spider = {}
-M[3].spider.PosiX = 190
+M[3].spider.PosiX = 230
 M[3].spider.PosiY = 150
 M[3].spider.LegExists = {1,1,1,1,1,1,1,1}
 
 M[3].collider = {}
-M[3].collider.GroupX = {40, 550}
+M[3].collider.GroupX = {90, 550}
 M[3].collider.GroupY = {280, 410}
-M[3].collider.Num = {5, 8}
+M[3].collider.Num = {4, 8}
 M[3].collider.Orientation = {1, 1}
 
 M[3].goal = {}

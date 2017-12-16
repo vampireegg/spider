bf98c77224d155dcc228dcf2a7f74f62f688c14b
nayeem <vampireegg@gmail.com>
2017-12-16 16:19:01 +0600

    dos_donts

--- spider/dos_donts.lua
+++ spider/dos_donts.lua
@@ -2,15 +2,87 @@
 local commonProp = require("commonProp")
 local levelProp = require("levelProp")
 local composer = require( "composer" )
+local scene = composer.newScene()
+
 
 local Level
+local bgProp = {}
+local totalHeight
+local totalWidth
+
+local function gotoGame()
+    composer.gotoScene( "level" )
+end
 
 function scene:create( event )
  
+	Level = composer.getVariable("level")
+	
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
+	
+	totalWidth = commonProp.total.Width
+	totalHeight = commonProp.total.Height
  
-    local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
+	bgProp.Img = levelProp[Level].dos_donts.Img
+	bgProp.Color = levelProp[Level].dos_donts.Color
+    local background = display.newImageRect( sceneGroup, bgProp.Img, totalHeight, totalWidth )
     background.x = display.contentCenterX
     background.y = display.contentCenterY
+	--background:setFillColor(bgProp.Color)
+	
+	background:addEventListener( "tap", gotoGame )
 end
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene

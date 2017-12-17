a57028f6ab3a2e982332694bbec87ea5600cccd3
nayeem <vampireegg@gmail.com>
2017-12-17 23:36:45 +0600

    some portal posi fixed

--- spider/levelProp.lua
+++ spider/levelProp.lua
@@ -310,9 +310,9 @@ M[8].dos_donts.Color = {0.2, 0.2, 0.2, 1}
 M[9] = {}
 
 M[9].spider = {}
-M[9].spider.MyScale = 0.12
-M[9].spider.PosiX = 320
-M[9].spider.PosiY = 100
+M[9].spider.MyScale = 0.11
+M[9].spider.PosiX = 340
+M[9].spider.PosiY = 80
 M[9].spider.LegExists = {1,1,1,1,1,1,1,1}
 
 M[9].collider = {}
@@ -336,8 +336,8 @@ M[9].eye.Opacity = 0
 M[9].portal = {}
 M[9].portal.Exists = 1
 M[9].portal.Types = {6, 2, 3}
-M[9].portal.PosiX = {{320, 740}, {220, EOF}, {850, 740}}
-M[9].portal.PosiY = {{210, 480}, {100, EOF}, {480, 100}}
+M[9].portal.PosiX = {{340, 720}, {200, EOF}, {870, 720}}
+M[9].portal.PosiY = {{230, 500}, {80, EOF}, {500, 80}}
 
 M[9].dos_donts = {}
 M[9].dos_donts.Img = "dos_donts_8.png"

2e2deaf93d78ece467d1e9c790c924a0a50e8823
nayeem <vampireegg@gmail.com>
2017-12-26 18:01:38 +0600

    mini fix

--- spider/main.lua
+++ spider/main.lua
@@ -36,5 +36,6 @@ local options = {
  
 -- Go to the menu screen
 composer.setVariable( "1st_level", 1 )
+composer.setVariable( "level", 1 )
 composer.setVariable( "max_completed_level", levelTable[1] )
 composer.gotoScene( "select_level", options )

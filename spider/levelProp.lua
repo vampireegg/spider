36e66ce3cb2cbb43b58196c3b3c82a9a32b9da6b
nayeem <vampireegg@gmail.com>
2017-12-26 18:28:07 +0600

    more portal

--- spider/levelProp.lua
+++ spider/levelProp.lua
@@ -457,9 +457,9 @@ M[11].eye.Opacity = 0
 
 M[11].portal = {}
 M[11].portal.Exists = 1
-M[11].portal.Types = {5}
-M[11].portal.PosiX = {{458,OOF}}
-M[11].portal.PosiY = {{63,OOF}}
+M[11].portal.Types = {5, 2}
+M[11].portal.PosiX = {{458,OOF}, {508,-OOF}}
+M[11].portal.PosiY = {{63, OOF}, {253,-OOF}}
 
 M[11].heartExists = 1
 M[11].heart = {}

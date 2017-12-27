c9250bdda6f46c0237ac5f788e7b8ce0a82a2fd9
nayeem <vampireegg@gmail.com>
2017-12-27 12:48:47 +0600

    li changed

--- spider/main.lua
+++ spider/main.lua
@@ -38,4 +38,4 @@ local options = {
 composer.setVariable( "1st_level", 1 )
 composer.setVariable( "level", 11 )
 composer.setVariable( "max_completed_level", levelTable[1] )
-composer.gotoScene( "level", options )
+composer.gotoScene( "select_level", options )

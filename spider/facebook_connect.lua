0956bc2fcbe38e614da96cf0cc831cc05a75bacc
nayeem <vampireegg@gmail.com>
2018-01-10 14:44:58 +0600

    more changes

--- spider/facebook_connect.lua
+++ spider/facebook_connect.lua
@@ -56,6 +56,7 @@ function scene:create( event )
 	control.Time1stTime = composer.getVariable("Time1stTime")
 	control.Completed_level = composer.getVariable("Completed_level")
 	control.UsedFreeMoves = composer.getVariable("UsedFreeMoves")
+	local Scale = 0.04
 	
     local sceneGroup = self.view
 	
@@ -83,19 +84,44 @@ function scene:create( event )
 	background = display.newRect(sceneGroup, totalHeight/2, totalWidth/2, totalHeight, totalWidth)
 	background:setFillColor(control.rectColor[1], control.rectColor[2], control.rectColor[3], control.rectColor[4])
 	
+	
 	local posiX = totalHeight/2
 	local posiY = totalWidth/2 - 200
 	
 	background.txt = {}
 	background.txt[1] = display.newText( "Congratulations ", posiX, posiY,  "comic.ttf", 36 )
 	background.txt[1]:setFillColor( 0.9, 0.9, 0.65, 1)
+	sceneGroup:insert(background.txt[1])
 	
 	posiY = posiY + 100
 	
-	background.txt[2] = display.newText( "You completed the Hard Level " .. control.Completed_level .. " in " .. control.Time1stTime .. " seconds, can your friends beat you?", posiX, posiY,  "comic.ttf", 16 )
+	background.txt[2] = display.newText( "You completed the Hard Level " .. control.Completed_level .. " in " .. control.Time1stTime .. " seconds, challenge friends on FaceBook?", posiX, posiY,  "comic.ttf", 16 )
 	background.txt[2]:setFillColor( 0.9, 0.9, 0.65, 1)
+	sceneGroup:insert(background.txt[2])
 
 	background:addEventListener("tap", gotoGame)
+	
+	posiY = posiY + 100
+	
+	background.txt[3] = display.newText( "Confirm: ", posiX - 50, posiY,  "comic.ttf", 20 )
+	background.txt[3]:setFillColor( 0.6, 0.6, 1, 1)
+	sceneGroup:insert(background.txt[3])
+	
+	background.facebookButton = display.newImageRect( sceneGroup, "fb_button.png", 1920, 1040 )
+	background.facebookButton.x = background.txt[3].x + background.txt[3].width/2 + 20
+	background.facebookButton.y = posiY
+	background.facebookButton:scale(Scale, Scale)
+	
+	posiY = posiY + 100
+	
+	background.txt[4] = display.newText( "Cancel : ", posiX - 50, posiY,  "comic.ttf", 20 )
+	background.txt[4]:setFillColor( 0.9, 0.9, 0.3, 1)
+	sceneGroup:insert(background.txt[4])
+	
+	background.crossButton = display.newImageRect( sceneGroup, "cross2.png", 70, 65 )
+	background.crossButton.x = background.txt[4].x + background.txt[4].width/2 + 20
+	background.crossButton.y = posiY
+	background.crossButton:scale(.5, .5)
 	
 	
 

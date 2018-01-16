d1f3ab60e72b8978dde966f45f35bec3cd126aae
nayeem <vampireegg@gmail.com>
2018-01-16 15:12:12 +0600

    new ids added

--- spider/level.lua
+++ spider/level.lua
@@ -10,7 +10,10 @@
 -- admob ad unit id: ca-app-pub-3541871007849876/3951117876
 -- startapp ad id: 200202265
 -- facebook app id: 406399589814193
---KEY HASH: E6H9jBE7rr5S50MYNeiOg0p1bkQ=
+-- vungle app id: 5a5dc14aef493e900c002889
+-- vungle placement reference id: DEFAULT74324
+-- vungle reporting api id: 5a5dc14aef493e900c002889
+-- KEY HASH: E6H9jBE7rr5S50MYNeiOg0p1bkQ=
 
 
 local physics = require( "physics" )
